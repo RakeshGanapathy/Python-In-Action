5e629ea2bba5f2c657bfe8480f2c4fe446932bf0
Rakesh Ganapathy <rakesh.ganapathy@gmail.com>
2021-11-23 12:50:47 +0530

    core python

---
+++ PycharmProjects/Core/variables/variable.py
@@ -0,0 +1,17 @@
+name = "venkat prabu";
+trip = "goa";
+print(name + '\'s holiday' + " in " + trip);
+
+x = 1;
+y = 3;
+z = x + y;
+print(z);
+print(name + '\'s party')  # venkat prabu's party
+# index out of bound
+# print(name[100])
+
+print(name[1:])  # enkat prabu
+print(name[10]) # b
+print(name[1:10])  # enkat prab
+print(len(name))
+
